a6358c484140ea10e6053bbf0e88603e2e2ad0bc
exoream <shiigetsu1@gmail.com>
2024-11-07 19:02:43 +0800

    feat: add create product controller

---
+++ features/product/controller/controller.go
@@ -0,0 +1,48 @@
+package controller
+
+import (
+	"skripsi/features/product/dto/request"
+	"skripsi/features/product/interfaces"
+	"skripsi/utils/constant"
+	"skripsi/utils/helper"
+	"skripsi/utils/jwt"
+
+	"github.com/labstack/echo/v4"
+)
+
+type productController struct {
+	productService interfaces.ProductServiceInterface
+}
+
+func NewProductController(productService interfaces.ProductServiceInterface) *productController {
+	return &productController{
+		productService: productService,
+	}
+}
+
+func (p *productController) Create(e echo.Context) error {
+	_, role, errExtract := jwt.ExtractToken(e)
+	if role != "admin" {
+		return helper.ResponseError(401, constant.ERROR_AKSES_ROLE)
+	}
+
+	if errExtract != nil {
+		return errExtract
+	}
+
+	input := request.ProductRequest{}
+
+	errBind := e.Bind(&input)
+	if errBind != nil {
+		return helper.ResponseError(400, "invalid input data")
+	}
+
+	data := request.ProductRequestToProductCore(input)
+
+	_, err := p.productService.Create(data)
+	if err != nil {
+		return err
+	}
+
+	return e.JSON(200, helper.ResponseSuccess(constant.SUCCESS_CREATE_DATA))
+}
