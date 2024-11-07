fe16fa63c3fc3ed2bb1011b7c1e1d60748d1640f
exoream <shiigetsu1@gmail.com>
2024-11-07 18:19:22 +0800

    feat: add interfaces in product

---
+++ features/product/interfaces/interfaces.go
@@ -0,0 +1,22 @@
+package interfaces
+
+import (
+	"skripsi/features/product/entity"
+	"skripsi/utils/pagination"
+)
+
+type ProductRepositoryInterface interface {
+	Create(data entity.ProductCore) (entity.ProductCore, error)
+	GetAll(search string, page, limit int) ([]entity.ProductCore, pagination.PageInfo, int, error)
+	GetById(id string) (entity.ProductCore, error)
+	UpdateById(id string, data entity.ProductCore) error
+	DeleteById(id string) error
+}
+
+type ProductServiceInterface interface {
+	Create(data entity.ProductCore) (entity.ProductCore, error)
+	GetAll(search string, page, limit int) ([]entity.ProductCore, pagination.PageInfo, int, error)
+	GetById(id string) (entity.ProductCore, error)
+	UpdateById(id string, data entity.ProductCore) error
+	DeleteById(id string) error
+}
