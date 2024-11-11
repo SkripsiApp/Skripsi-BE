828e7db1523d375230dcf471cdcb174878889705
exoream <shiigetsu1@gmail.com>
2024-11-12 00:44:44 +0800

    fix: response status in register user

--- features/user/controller/controller.go
+++ features/user/controller/controller.go
@@ -35,7 +35,7 @@ func (uh *userController) Register(e echo.Context) error {
 		return err
 	}
 
-	return e.JSON(200, helper.ResponseSuccess(constant.SUCCESS_CREATE_DATA))
+	return e.JSON(201, helper.ResponseSuccess(constant.SUCCESS_CREATE_DATA))
 }
 
 func (uh *userController) GetAll(e echo.Context) error {
